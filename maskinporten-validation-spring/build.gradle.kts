75aa5d17d7884152d3fb1df4d4f25271fb86e3a7
TheMather1 <the.mather1@gmail.com>
2021-06-16 16:18:05 +0200

    Disable MaskinportenValidatorConfigurer when MaskinportenValidatorAutoConfigurer is present.

--- maskinporten-validation-spring/build.gradle.kts
+++ maskinporten-validation-spring/build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
     implementation("javax.servlet", "javax.servlet-api", "4.0.1")
     implementation("org.springframework", "spring-webmvc")
     implementation("org.springframework.boot", "spring-boot")
+    implementation("org.springframework.boot", "spring-boot-autoconfigure")
     implementation("org.slf4j", "slf4j-api", "1.7.30")
     testImplementation("org.junit.jupiter", "junit-jupiter", "5.7.0")
     testImplementation("org.springframework.boot", "spring-boot-starter-test") {
--- maskinporten-validation-spring/src/main/kotlin/no/nav/pensjonsamhandling/maskinporten/validation/config/MaskinportenValidatorConfigurer.kt
+++ maskinporten-validation-spring/src/main/kotlin/no/nav/pensjonsamhandling/maskinporten/validation/config/MaskinportenValidatorConfigurer.kt
@@ -5,6 +5,7 @@ import no.nav.pensjonsamhandling.maskinporten.validation.interceptor.Maskinporte
 import no.nav.pensjonsamhandling.maskinporten.validation.interceptor.MaskinportenValidatorInterceptorConfigurer
 import no.nav.pensjonsamhandling.maskinporten.validation.orgno.RequestAwareOrganisationValidator
 import no.nav.pensjonsamhandling.maskinporten.validation.orgno.RequestAwareOrganisationValidator.NoopOrganisationValidator
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -14,6 +15,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @EnableWebMvc
 @Configuration
 @EnableConfigurationProperties(MaskinportenValidatorProperties::class)
+@ConditionalOnMissingClass("no.nav.pensjonsamhandling.maskinporten.validation.test.MaskinportenValidatorAutoConfiguration")
 class MaskinportenValidatorConfigurer(
     internal val properties: MaskinportenValidatorProperties,
 ) : WebMvcConfigurer {
